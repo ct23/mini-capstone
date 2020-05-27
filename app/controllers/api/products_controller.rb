05df05d487d1dfff685427d5e6ed5d90e0b3da8a
Colin <colintklein@gmail.com>
2020-05-27 13:18:40 -0500

    Adds routes, controller methods, and json pages to access individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,20 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'all_products.json.jb'
   end
+  def product1_action
+    @product = Product.find_by(id: 1)
+    render 'product.json.jb'
+  end
+  def product2_action
+    @product = Product.find_by(id: 2)
+    render 'product.json.jb'
+  end
+  def product3_action
+    @product = Product.find_by(id: 3)
+    render 'product.json.jb'
+  end
+  def product4_action
+    @product = Product.find_by(id: 4)
+    render 'product.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/all_products" => "products#all_products_action"
+    get "/product1" => "products#product1_action"
+    get "/product2" => "products#product2_action"
+    get "/product3" => "products#product3_action"
+    get "/product4" => "products#product4_action"
   end
+
 end
