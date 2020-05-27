0305c9aac092bf6b3479621083aa5d826ae58c8c
Colin <colintklein@gmail.com>
2020-05-27 12:47:58 -0500

    Creates route to display JSON for  all available products;2C

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace: api do
+    get "/all_products" => "products#all_products_action"
+  end
 end
