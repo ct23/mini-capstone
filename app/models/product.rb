7cbdc12fdd024b5da2fb8e072f45ea0fa3834bd2
Colin <colintklein@gmail.com>
2020-06-04 11:32:42 -0500

    Changes Product model to include in_stock boolean, changes description to text, changes price to decimal. Adds Product methods and updates views to utilize those.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+
+  def is_discounted?
+    if price < 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def tax
+    # Assume 9% tax rate
+    return (price * 0.09).round(2)
+  end
+
+  def total
+    return ('%.2f' % (tax + price)).to_i
+  end
+
 end
