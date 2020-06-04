bed138093a7dba3c6fc1dee2444489835887f266
Colin <colintklein@gmail.com>
2020-06-04 11:44:27 -0500

    Updates controller to reflect new in_stock attribute for Product model

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -41,7 +41,8 @@ class Api::ProductsController < ApplicationController
       name: params['name'],
       price: params['price'],
       image_url: params['image_url'],
-      description: params['description']
+      description: params['description'],
+      in_stock: params['in_stock']
     )
     render 'show_product.json.jb'
   end
@@ -55,6 +56,7 @@ class Api::ProductsController < ApplicationController
     @product.price = params[:price] || @product.price
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
+    @product.in_stock = params[:instock] || @product.in_stock
     # save product
     @product.save
     # render a view
