b14c9be31330e736b8e4add748214e67acba39e6
Colin <colintklein@gmail.com>
2020-06-17 16:00:31 -0500

    Adds products controller (no namespace) and index, edit, new, and show routes and methods, corresponding views

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  get "/products" => "products#index"
+  get "/products/new" => "products#new"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  get "/products/:id/edit" => "products#edit"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
 
   namespace :api do
-    # get "/all_products" => "products#all_products_action"
-    # get "/product1" => "products#product1_action"
-    # get "/product2" => "products#product2_action"
-    # get "/product3" => "products#product3_action"
-    # get "/product4" => "products#product4_action"
-    # # create a single route that can display ANY single product using a query parameter
-    # get "/getproduct" => "products#product_param_action"
-    # # create a single route that can display ANY single product using a url segment parameter
-    # get "/getproduct/:prod_id" => "products#product_param_action"
 
     # New REST paths to replace above routes
     get "/products" => "products#index"
